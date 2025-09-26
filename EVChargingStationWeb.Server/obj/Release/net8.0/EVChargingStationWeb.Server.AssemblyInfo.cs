3b1d2c5e37d13302528454856528709b9a09883a
shenori <shenorikaushalya209@gmail.com>
2025-09-26 21:43:44 +0530

    Restructure repo: group web app into EVChargingStationWeb folder

--- EVChargingStationWeb.Server/obj/Release/net8.0/EVChargingStationWeb.Server.AssemblyInfo.cs
+++ EVChargingStationWeb.Server/obj/Release/net8.0/EVChargingStationWeb.Server.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("EVChargingStationWeb.Server")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96ac91fd5ec0f2e207a195cacee08775f407fda3")]
 [assembly: System.Reflection.AssemblyProductAttribute("EVChargingStationWeb.Server")]
